ecbe2b14d8dd6e3a4c17d4978559a3cbdc0f0a7c
noguchi <nogulin0530@gmail.com>
2023-05-30 00:53:04 -0700

    パスワード認証の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,12 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice]="ユーザー登録が完了しました。"
       redirect_to users_path
     else
       render "new", status: :unprocessable_entity
     end
-  end
-
-  def create
-    @user = User.create(user_params)
-    @user.save
-    redirect_to users_path
   end
 
   def edit
@@ -33,25 +28,33 @@ class UsersController < ApplicationController
     @user.name = params[:name]
     @user.email = params[:email]
     @user.update(user_params)
+    flash[:notice] = "ユーザー情報を編集しました。"
     redirect_to users_path
   end
 
   def login_form
   end
   def login
-    @user = User.find_by(email: params[:imail], password: params[:password])
+    @user = User.find_by(email: params[:email], password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to posts_path
     else
       @error_message = "メールアドレスまたはパスワードが間違っています。"
-      @email = params [:email]
+      @email = params[:email]
       @password = params[:password]
       render "login_form", status: :unprocessable_entity
     end
   end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to login_path
+  end
   private 
   def user_params
-    params.require(:user).permit(:name, :email, :image_name)
+    params.require(:user).permit(:name, :email, :image_name, :password)
   end
 end
