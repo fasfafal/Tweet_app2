42571e2e5265fa546ec6101f3a3ae43c7d76a64a
noguchi <nogulin0530@gmail.com>
2023-06-01 22:19:55 -0700

    migrateエラー

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     validates :name ,{presence: true}
-    validates :email ,{presence: true,uniqueness: true}
+    
     validates :password, {presence: true}
 end
